3901fd5c61616d3643749eb413c7439433d5bcc4
Mohd Ubaidullah <devubaid493@gmail.com>
2025-05-21 15:00:17 +0530

    first commit

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ app.use("/api/reviews", require("./routes/reviewRoutes"));
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on the port ${PORT}`));
